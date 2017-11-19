4878530c8d9bd5f850e0eda7748fdc8d68ef4c51
Wendy Beck <webeck@gmail.com>
2017-11-18 16:11:36 -0800

    google cloud function

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/*
-* HTTP Cloud Function.
-*
-* @param {Object} req Cloud Function request context.
-* @param {Object} res Cloud Function response context.
-*/
-export function chattyWishList(req, res) {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-
-  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  return res.send(JSON.stringify({ "speech": response, "displayText": response
-  //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
+exports.chattyWishList = function helloGET (req, res) {
+  res.send('Hello World!');
 };
-
-
-
