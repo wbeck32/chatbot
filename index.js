69b5d8195df57b6b62ee88f2b674f27058e1167b
Wendy Beck <webeck@gmail.com>
2017-11-20 15:18:25 -0800

    getting eBay api call to work

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-/**
- * Responds to any HTTP request that can provide a "message" field in the body.
- *
- * @param {!Object} req Cloud Function request context.
- * @param {!Object} res Cloud Function response context.
- */
-exports.chattyWishList = function helloWorld(req, res) {
-  // Example input: {"message": "Hello!"}
-  if (req.body.message === undefined) {
-    // This is an error case, as "message" is required.
-    res.status(400).send('No message defined!');
-  } else {
-    // Everything is okay.
-    console.log(req.body.message);
-    res.status(200).send('Success: ' + req.body.message);
-  }
+'use strict';
+require('dotenv');
+const req = require('superagent');
+
+exports.chattyWishList = async (req, res) => {
+  console.log(2, req);
+  const data = 'bicycle';
+  const results = await callFindingAPI(data);
+  console.log(1, results);
+};
+
+const callFindingAPI = async keywords => {
+  const searchResults = await req
+    .get('https://svcs.ebay.com/services/search/FindingService/v1')
+    .send({
+      'OPERATION-NAME': 'findItemsByKeywords',
+      'SERVICE-VERSION': '1.0.0',
+      'RESPONSE-DATA-FORMAT': 'JSON',
+      'keywords': keywords,
+      'SECURITY-APPNAME': process.env.SECURITY-APPNAME,
+      'GLOBAL-ID': 'EBAY-US'
+    });
+
+  console.log(searchResults);
+  return searchResults;
 };
