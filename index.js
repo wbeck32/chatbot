62b9deab7d81e5429de6667ddf947e490c50bb54
Wendy Beck <webeck@gmail.com>
2017-11-20 20:50:38 -0800

    wiring up the API

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ const req = require('superagent');
 
 exports.chattyWishList = (req, res) => {
   console.log(2, req);
-  const data = 'bicycle';
+  const data = 'arry+poter+phonix';
+  const refinedKeywords = checkKeywords(data);
   callFindingAPI(data)
   .then(results => {
     console.log(1, results);
 
   });
 };
+
+function checkKeywords(keywords) {
+  return req
+  .get('https://svcs.ebay.com/services/search/FindingService/v1')
+  .send({
+    'OPERATION-NAME': 'getSearchKeywordsRecommendation',
+    'SERVICE-VERSION': '1.0.0',
+    'RESPONSE-DATA-FORMAT': 'JSON',
+    'keywords': keywords,
+    'SECURITY-APPNAME': process.env.SECURITY-APPNAME,
+    'GLOBAL-ID': 'EBAY-US'
+  })
+  .then(refinedKeywords => {
+    console.log(refinedKeywords);
+    return refinedKeywords;
+
+  })
+
+
+}
 
 function callFindingAPI(keywords) {
   return req
