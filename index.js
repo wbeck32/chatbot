a305058dcc6ff9dbdd95fb74ef343a14d9d21a0b
Wendy Beck <webeck@gmail.com>
2017-11-22 01:00:55 -0800

    syntax error does not computer

--- index.js
+++ index.js
@@ -2,57 +2,45 @@
 require('dotenv');
 const req = require('superagent');
 
-exports.chattyWishList = (req, res) => {
-
+exports.chattyWishList = async (req, res) => {
   console.log(2, req.body.queryResult.parameters.search_item);
   const data = req.body.queryResult.parameters.search_item;
-  const refinedKeywords = checkKeywords(data)
-  .then(refined => {
-    console.log(3, refined)
-    return refined;
-  });
-  const foundResults = callFindingAPI(refinedKeywords)
-  .then(results => {
-    console.log(4, results);
-    return results
-  });
+  const refinedKeywords = await checkKeywords(data);
+  console.log(3, refinedKeywords);
+  const foundResults = await callFindingAPI(refinedKeywords);
+  console.log(4, foundResults);
 };
 
-checkKeywords = async((keywords) =>  {
-  console.log(5, keywords)
+const checkKeywords = async keywords => {
+  console.log(5, keywords);
   const refinedKeywords = await req
-  .get('https://svcs.ebay.com/services/search/FindingService/v1')
-  .send({
-    'OPERATION-NAME': 'getSearchKeywordsRecommendation',
-    'SERVICE-VERSION': '1.0.0',
-    'RESPONSE-DATA-FORMAT': 'JSON',
-    'keywords': keywords,
-    'SECURITY-APPNAME': process.env._,
-    'APPNAME': process.env._,
-    'GLOBAL-ID': 'EBAY-US'
-  })
+    .get('https://svcs.ebay.com/services/search/FindingService/v1')
+    .send({
+      'OPERATION-NAME': 'getSearchKeywordsRecommendation',
+      'SERVICE-VERSION': '1.0.0',
+      'RESPONSE-DATA-FORMAT': 'JSON',
+      keywords: keywords,
+      'SECURITY-APPNAME': process.env._,
+      APPNAME: process.env._,
+      'GLOBAL-ID': 'EBAY-US'
+    });
   console.log(6, refinedKeywords);
   return refinedKeywords;
+};
 
-})
-
-callFindingAPI = async((refinedKeywords) => {
-  console.log(7, refinedKeywords)
+const callFindingAPI = async refinedKeywords => {
+  console.log(7, refinedKeywords);
   const searchResults = await req
     .get('https://svcs.ebay.com/services/search/FindingService/v1')
     .send({
       'OPERATION-NAME': 'findItemsByKeywords',
       'SERVICE-VERSION': '1.0.0',
       'RESPONSE-DATA-FORMAT': 'JSON',
-      'keywords': refinedKeywords,
+      keywords: refinedKeywords,
       'SECURITY-APPNAME': process.env._,
-      'APPNAME': process.env._,
+      APPNAME: process.env._,
       'GLOBAL-ID': 'EBAY-US'
-    })
-
-      console.log(8, searchResults);
-      return searchResults;
-
-
-
-})
+    });
+  console.log(8, searchResults);
+  return searchResults;
+};
