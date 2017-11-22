3226634b31aa5537774abea293ece245af4ad5ce
Wendy Beck <webeck@gmail.com>
2017-11-22 08:59:18 -0800

    finally did some good debugging

--- index.js
+++ index.js
@@ -1,61 +1,78 @@
 'use strict';
 require('dotenv');
 const req = require('superagent');
+const querystring = require('querystring');
 
 exports.chattyWishList = (req, res) => {
+  const queryString = {
+    session:
+      'projects/chatbot-search-4bfd3/agent/sessions/0ee56394-3e4d-4dbe-af8e-08b629be9c0b',
+    responseId: '3cb18e49-a58e-4df4-857d-1cd037e6fc28',
+    queryResult: {
+      parameters: {
+        zip_code: '87890',
+        local_search: 'nearby',
+        budget: {
+          amount: 40,
+          currency: 'USD'
+        },
+        condition: 'new',
+        search_item: 'bickcle'
+      }
+    }
+  };
+  // console.log(2, req.queryResult.parameters.search_item)
+  const data = queryString.queryResult.parameters.search_item;
+  // const data = 'arry+poter+phonix';
 
-  console.log(2, req.body.outputContexts)
-  const data = req.body.queryResult.parameters.search_item;
-  const refinedKeywords = checkKeywords(data)
-  .then(refined => {
-    console.log(3, refined)
-    return refined;
-  });
-  const foundResults = callFindingAPI(refinedKeywords)
-  .then(results => {
-    console.log(4, results);
-    return results
-  });
-  res.send(foundResults)
+  return checkKeywords(data)
+    .then(refined => {
+      console.log(3, refined.text);
+      return refined;
+    })
+    .then(refinedKeywords => {
+      return callFindingAPI(refinedKeywords).then(results => {
+        console.log(4, results.body);
+        res.send(results);
+      });
+    });
 };
 
-function checkKeywords(keywords) {
-  console.log(5, keywords)
-  return req
-  .get('https://svcs.ebay.com/services/search/FindingService/v1')
-  .send({
+const checkKeywords = data => {
+  console.log(5, data);
+  const query = querystring.stringify({
     'OPERATION-NAME': 'getSearchKeywordsRecommendation',
     'SERVICE-VERSION': '1.0.0',
     'RESPONSE-DATA-FORMAT': 'JSON',
-    'keywords': keywords,
-    'SECURITY-APPNAME': process.env._,
-    'APPNAME': process.env._,
+    keywords: data,
+    'SECURITY-APPNAME': 'WendyBec-chattywi-PRD-551ca6568-8ddbb013',
     'GLOBAL-ID': 'EBAY-US'
-  })
-  .then(refinedKeywords => {
-    console.log(6, refinedKeywords);
-    return refinedKeywords;
+  });
 
-  })
-}
-
-function callFindingAPI(keywords) {
-  console.log(7, keywords)
   return req
     .get('https://svcs.ebay.com/services/search/FindingService/v1')
-    .send({
-      'OPERATION-NAME': 'findItemsByKeywords',
-      'SERVICE-VERSION': '1.0.0',
-      'RESPONSE-DATA-FORMAT': 'JSON',
-      'keywords': keywords,
-      'SECURITY-APPNAME': process.env._,
-      'APPNAME': process.env._,
-      'GLOBAL-ID': 'EBAY-US'
-    })
+    .query(query)
+    .then(refinedKeywords => {
+      console.log(6, refinedKeywords.text);
+      return refinedKeywords;
+    });
+};
+
+const callFindingAPI = keywords => {
+  console.log(7, keywords.text);
+  const query2 = querystring.stringify({
+    'OPERATION-NAME': 'findItemsByKeywords',
+    'SERVICE-VERSION': '1.0.0',
+    'RESPONSE-DATA-FORMAT': 'JSON',
+    keywords: 'bicycle',
+    'SECURITY-APPNAME': 'WendyBec-chattywi-PRD-551ca6568-8ddbb013',
+    'GLOBAL-ID': 'EBAY-US'
+  });
+  return req
+    .get('https://svcs.ebay.com/services/search/FindingService/v1')
+    .query(query2)
     .then(searchResults => {
-      console.log(8, searchResults);
+      console.log(8, searchResults.text);
       return searchResults;
-
-    })
-
+    });
 };
