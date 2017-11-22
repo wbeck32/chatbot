d9c3fdf12c0fb549fc115cb0d11541ec8a8d6b53
Wendy Beck <webeck@gmail.com>
2017-11-21 18:43:24 -0800

    troubleshooting google cloud

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require('dotenv');
 const req = require('superagent');
 
 exports.chattyWishList = (req, res) => {
+  console.log(11111)
   response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response, "source" : response, "data" : {}, "contextOut" : [] }));
+  res.send(JSON.stringify({"sessionId":"0ee56394-3e4d-4dbe-af8e-08b629be9c0b", "speech": "response", "displayText": "response", "source" : "response", "data" : {}, "contextOut" : [], "query":"weather", "lang" : "en", "timezone": "America/Los_Angeles" }));
 
 
   // console.log(2, req);
