dd5dfe67d578bf70722d841ae352c3b18153b5d3
Wendy Beck <webeck@gmail.com>
2017-11-22 01:34:35 -0800

    no async/await

--- index.js
+++ index.js
@@ -2,47 +2,60 @@
 require('dotenv');
 const req = require('superagent');
 
-const chattyWishList = async (req, res) => {
+exports.chattyWishList = (req, res) => {
+
   console.log(2, req.body.queryResult.parameters.search_item);
   const data = req.body.queryResult.parameters.search_item;
-  const refinedKeywords = await checkKeywords(data);
-  console.log(3, refinedKeywords);
-  const foundResults = await callFindingAPI(refinedKeywords);
-  console.log(4, foundResults);
-}
-
-const checkKeywords = async keywords => {
-  console.log(5, keywords);
-  const refinedKeywords = await req
-    .get('https://svcs.ebay.com/services/search/FindingService/v1')
-    .send({
-      'OPERATION-NAME': 'getSearchKeywordsRecommendation',
-      'SERVICE-VERSION': '1.0.0',
-      'RESPONSE-DATA-FORMAT': 'JSON',
-      keywords: keywords,
-      'SECURITY-APPNAME': process.env._,
-      APPNAME: process.env._,
-      'GLOBAL-ID': 'EBAY-US'
-    });
-  console.log(6, refinedKeywords);
-  return refinedKeywords;
+  const refinedKeywords = checkKeywords(data)
+  .then(refined => {
+    console.log(3, refined)
+    return refined;
+  });
+  const foundResults = callFindingAPI(refinedKeywords)
+  .then(results => {
+    console.log(4, results);
+    return results
+  });
+  res.send(foundResults)
 };
 
-const callFindingAPI = async refinedKeywords => {
-  console.log(7, refinedKeywords);
-  const searchResults = await req
+function checkKeywords(keywords) {
+  console.log(5, keywords)
+  return req
+  .get('https://svcs.ebay.com/services/search/FindingService/v1')
+  .send({
+    'OPERATION-NAME': 'getSearchKeywordsRecommendation',
+    'SERVICE-VERSION': '1.0.0',
+    'RESPONSE-DATA-FORMAT': 'JSON',
+    'keywords': keywords,
+    'SECURITY-APPNAME': process.env._,
+    'APPNAME': process.env._,
+    'GLOBAL-ID': 'EBAY-US'
+  })
+  .then(refinedKeywords => {
+    console.log(6, refinedKeywords);
+    return refinedKeywords;
+
+  })
+}
+
+function callFindingAPI(keywords) {
+  console.log(7, keywords)
+  return req
     .get('https://svcs.ebay.com/services/search/FindingService/v1')
     .send({
       'OPERATION-NAME': 'findItemsByKeywords',
       'SERVICE-VERSION': '1.0.0',
       'RESPONSE-DATA-FORMAT': 'JSON',
-      keywords: refinedKeywords,
+      'keywords': keywords,
       'SECURITY-APPNAME': process.env._,
-      APPNAME: process.env._,
+      'APPNAME': process.env._,
       'GLOBAL-ID': 'EBAY-US'
-    });
-  console.log(8, searchResults);
-  return searchResults;
-};
+    })
+    .then(searchResults => {
+      console.log(8, searchResults);
+      return searchResults;
 
-module.exports = chattyWishList
+    })
+
+};
