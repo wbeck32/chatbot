75f8badcf6ef199c4b1de89fc38a152991dc2f81
Wendy Beck <webeck@gmail.com>
2017-11-22 00:32:24 -0800

    console log all the things

--- index.js
+++ index.js
@@ -3,27 +3,23 @@ require('dotenv');
 const req = require('superagent');
 
 exports.chattyWishList = (req, res) => {
-  console.log(11111, process.env)
-  // const response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-  // res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  // res.send(JSON.stringify({"sessionId":"0ee56394-3e4d-4dbe-af8e-08b629be9c0b", "speech": response, "displayText": "response", "source" : "response", "data" : {}, "contextOut" : [], "query":"weather", "lang" : "en", "timezone": "America/Los_Angeles" }));
 
-
-  console.log(2, req);
+  console.log(2, req.body);
   const data = 'arry+poter+phonix';
   const refinedKeywords = checkKeywords(data)
   .then(refined => {
     console.log(3, refined)
-    return refined;
+    res.send(refined);
   });
   const foundResults = callFindingAPI(refinedKeywords)
   .then(results => {
-    console.log(1, results);
+    console.log(4, results);
     res.send(results)
   });
 };
 
 function checkKeywords(keywords) {
+  console.log(5, keywords)
   return req
   .get('https://svcs.ebay.com/services/search/FindingService/v1')
   .send({
@@ -36,7 +32,7 @@ function checkKeywords(keywords) {
     'GLOBAL-ID': 'EBAY-US'
   })
   .then(refinedKeywords => {
-    console.log(refinedKeywords);
+    console.log(6, refinedKeywords);
     res.send(refinedKeywords);
 
   })
@@ -45,6 +41,7 @@ function checkKeywords(keywords) {
 }
 
 function callFindingAPI(keywords) {
+  console.log(7, keywords)
   return req
     .get('https://svcs.ebay.com/services/search/FindingService/v1')
     .send({
@@ -57,7 +54,7 @@ function callFindingAPI(keywords) {
       'GLOBAL-ID': 'EBAY-US'
     })
     .then(searchResults => {
-      console.log(searchResults);
+      console.log(8, searchResults);
       res.send(searchResults);
 
     })
